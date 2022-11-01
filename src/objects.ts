7fde3644b1bd8f1e5aeea904c5ab97e2121659f6
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-01 10:36:03 +0000

    pseudocode

---
+++ src/objects.ts
@@ -0,0 +1,19 @@
+/*Write a function, `printAllKeyValuePairs(obj)` which takes an object 
+with unknown content and prints to console, one per line, each **key** and **value** in the object.
+
+e.g.:
+
+Given the following object as input,
+
+const characters = {
+  Bruce: "Hulk",
+  Peter: "Spiderman",
+  Ororo: "Storm"
+};
+*/
+
+/*
+    function printAllKeyValuePairs(obj)
+
+   console.log( obj. entries )
+*/
